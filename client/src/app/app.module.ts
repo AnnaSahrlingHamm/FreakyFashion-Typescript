32db13d7eb7e062e4e34a45ba258f105be058a66
Anna Sahrling-Hamm <anna.sahrling.hamm@gmail.com>
2025-08-15 15:47:02 +0200

    Fixar importfel etc.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,18 +8,39 @@ import { AppRoutingModule } from './app-routing.module'; // Se till att du har d
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { ProductCardComponent } from './shared/components/product-card/product-card.component';
-import { ProductCarouselComponent } from './shared/components/product-carousel/product-carousel.component';
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { HeroComponent } from './shared/components/hero/hero.component';
+import { SpotsComponent } from './shared/components/spots/spots.component';
 import { NotFoundComponent } from './search/not-found/not-found.component';
 import { AdminModule } from './admin/admin.module';
+import { FooterComponent } from './shared/components//footer/footer.component';
+import { ProductDetailsComponent } from './products/product-details/product-details.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AddProductComponent } from './admin/pages/add-product/add-product.component';
+import { AllProductsComponent } from './admin/pages/all-products/all-products.component';
+import { IconLinksComponent } from './shared/components/icon-links/icon-links.component';
+
+
 // Lägg till fler här när du bygger vidare
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FooterComponent,
+    IconLinksComponent,
+    NavBarComponent,
+    HeroComponent,
+    SpotsComponent,
     ProductCardComponent,
-    ProductCarouselComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    ProductDetailsComponent,
+    SearchResultsComponent,
+    HomeComponent,
+    AddProductComponent,
+    AllProductsComponent
+
     // Lägg till andra komponenter här
   ],
   imports: [
--- client/src/app/shared/components/icon-links/icon-links.component.ts
+++ client/src/app/shared/components/icon-links/icon-links.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faGlobeAmericas, faPlane, faShieldAlt, faSmile } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-icon-links',
-  imports: [],
   templateUrl: './icon-links.component.html',
-  styleUrl: './icon-links.component.css'
+  styleUrls: ['./icon-links.component.css']
 })
 export class IconLinksComponent {
-
+  constructor(private library: FaIconLibrary) {
+    library.addIcons(faGlobeAmericas, faPlane, faShieldAlt, faSmile);
+  }
 }
